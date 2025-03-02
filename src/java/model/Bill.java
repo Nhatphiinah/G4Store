1994eeaf63970461eb6f8e25a7c620ce5348dcaa
NhatPhi <phinguyen0905@gmail.com>
2025-03-02 23:26:48 +0700

    G4Store-P1-PhiNVN- Add model package

---
+++ src/java/model/Bill.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+/**
+ *
+ * @author Doanh
+ */
+
+import java.sql.Date;
+
+
+public class Bill {
+    int bill_id;
+    User user;
+    Float total;
+    String payment;
+    String address;
+    Date date;
+    int phone;
+    int status;
+    public Bill() {
+    }
+
+
+    public Bill(int bill_id, User user, Float total, String payment, String address, Date date, int phone, int status) {
+        this.bill_id = bill_id;
+        this.user = user;
+        this.total = total;
+        this.payment = payment;
+        this.address = address;
+        this.date = date;
+        this.phone = phone;
+        this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    
+
+    public int getBill_id() {
+        return bill_id;
+    }
+
+    public void setBill_id(int bill_id) {
+        this.bill_id = bill_id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Float getTotal() {
+        return total;
+    }
+
+    public void setTotal(Float total) {
+        this.total = total;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    
+
+    
+
+    
+    
+    
+    
+}
