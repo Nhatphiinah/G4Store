4ef4a2980d2315f30cb28ceb2b617efea5641f57
PhuNhuan <phamphunhuan000@gmail.com>
2025-03-03 01:05:03 +0700

    G4Store-P1-NhuanPP-OrderManager-Update BillDAO.java and Products.java

--- src/java/model/Product.java
+++ src/java/model/Product.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 /**
  *
- * @author 
+ * @author Doanh
  */
 
 public class Product {
@@ -26,6 +26,31 @@ public class Product {
     Product_Active active;
 
     public Product() {
+    }
+
+    public Product(Category cate, String product_id, String product_name, Float product_price, String product_describe, int quantity, String img) {
+        this.cate = cate;
+        this.product_id = product_id;
+        this.product_name = product_name;
+        this.product_price = product_price;
+        this.product_describe = product_describe;
+        this.quantity = quantity;
+        this.img = img;
+    }
+    
+    public Product(String product_id, String product_name, Float product_price, String product_describe, int quantity, String img) {
+        this.product_id = product_id;
+        this.product_name = product_name;
+        this.product_price = product_price;
+        this.product_describe = product_describe;
+        this.quantity = quantity;
+        this.img = img;
+    }
+    
+    public Product(String product_id, String product_name, String img) {
+        this.product_id = product_id;
+        this.product_name = product_name;
+        this.img = img;
     }
 
     public Category getCate() {
