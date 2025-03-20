4e268a2590d62a8ad24ec148753e262c611ce862
phusleeeee <lebaophu456@gmai;.com>
2025-03-20 23:41:35 +0700

    G4Store-P2-PhuLTB- update GetUserProductInCart

--- src/java/dal/cartDAO.java
+++ src/java/dal/cartDAO.java
@@ -95,7 +95,51 @@ public class cartDAO {
         }
         return null;
     }
-    
+   public Cart GetUserProductInCart(String productId, String userId){
+        String sql = "SELECT [cart_id]\n" +
+                    "      ,[product_id]\n" +
+                    "      ,[product_name]\n" +
+                    "      ,[product_img]\n" +
+                    "      ,[product_price]\n" +
+                    "      ,[total]\n" +
+                    "      ,[quantity]\n" +
+                    "      ,[user_id]\n" +
+                    "  FROM [ShopYouAndMeVersionFinal].[dbo].[cart]\n" +
+                    "  Where product_id = ? And user_id = ?";
+        
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, productId);
+            ps.setString(2, userId);
+            rs = ps.executeQuery();
+            if(rs.next()) {
+              Cart c = new Cart();
+              c.setId(rs.getInt("cart_id"));
+              c.setUserId(rs.getString("user_id"));
+              
+              List<Item> items = new ArrayList<>();
+              
+              Item item = new Item();
+              Product p = new Product();
+              p.setProduct_id(rs.getString("product_id"));
+              p.setProduct_name(rs.getString("product_name"));
+              p.setImg(rs.getString("product_img"));
+              p.setProduct_price(rs.getFloat("product_price"));
+              
+              item.setProduct(p);
+              item.setQuantity(rs.getInt("quantity"));
+              
+              items.add(item);
+              items.add(item);
+              c.setItems(items);
+              
+              return c;
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }  
     
     
 }
