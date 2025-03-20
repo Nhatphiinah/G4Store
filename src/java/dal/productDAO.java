d14c0231df4f63614c432bc09d963d077820c8c2
trongnhan <trongnhan20092004@gmail.com>
2025-03-20 22:16:04 +0700

    G4Store-P2-NhanPNT-update search product by category

--- src/java/dal/productDAO.java
+++ src/java/dal/productDAO.java
@@ -554,7 +554,7 @@ public class productDAO extends DBContext {
         }
         return arr;
     }
-
+    //NhanPNTCE180453-search product by category
     public List<Product> getProductByCategory(int category_id) {
         List<Product> list = new ArrayList<>();
         String sql = "select c.category_name , p.product_id , p.product_name, p.product_price, p.product_describe, p.quantity,p.img from product p inner join category c on p.category_id = c.category_id inner join product_active pa on pa.product_id = p.product_id Where pa.active ='True' And p.category_id=?";
