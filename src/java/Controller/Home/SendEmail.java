67b7fb4acc255ccb533b06dcf68178e6a4ca52b6
NhatPhi <phinguyen0905@gmail.com>
2025-03-29 13:31:30 +0700

    G4Store-P1-PhiNVN- edit send email to Dq######

--- src/java/Controller/Home/SendEmail.java
+++ src/java/Controller/Home/SendEmail.java
@@ -19,18 +19,18 @@ import javax.mail.internet.MimeMessage;
  * @author
  */
 public class SendEmail {
-    
+
     // pass:hqhg edip bajd foln
     public String getRandom() {
         Random rnd = new Random();
         int number = rnd.nextInt(999999);
-        return String.format("%06d", number);
+               return "Dq" + String.format("%06d", number);
     }
 
     public boolean sendEmail1(UserC userc) {
         boolean test = false;
-        String fromEmail = "vng115123@gmail.com";
-        String password = "Dinhquoc123";
+        String fromEmail = "anpk2300@gmail.com";
+        String password = "hqhg edip bajd foln";
         String toEmail = userc.getEmail();
         try {
             Properties pr = new Properties();
@@ -62,8 +62,8 @@ public class SendEmail {
     }
     public boolean sendEmail2(UserC userc) {
         boolean test = false;
-        String fromEmail = "vng115123@gmail.com";
-        String password = "Dinhquoc123";
+        String fromEmail = "anpk2300@gmail.com";
+        String password = "hqhg edip bajd foln";
         String toEmail = userc.getEmail();
         try {
             Properties pr = new Properties();
@@ -86,6 +86,40 @@ public class SendEmail {
             mess.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
             mess.setSubject("User Email Verification");
             mess.setText("Update new password successfully.Please your account using this code: "+userc.getCode());
+            Transport.send(mess);
+            test = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return test;
+    }
+    
+    public boolean sendEmail3(String to, String subject, String content) {
+        boolean test = false;
+        String fromEmail = "anpk2300@gmail.com";
+        String password = "hqhg edip bajd foln";
+        String toEmail = to;
+        try {
+            Properties pr = new Properties();
+            pr.setProperty("mail.smtp.host", "smtp.gmail.com"); // Thay đổi thành máy chủ SMTP thực tế
+            pr.setProperty("mail.smtp.port", "587");
+            pr.setProperty("mail.smtp.auth", "true");
+            pr.setProperty("mail.smtp.starttls.enable", "true");
+            pr.setProperty("mail.smtp.socketFactory.port", "587");
+            pr.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            //get sesion
+            Session session;
+            session = Session.getInstance(pr, new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(fromEmail, password);
+                }
+            });
+            //mesage
+            Message mess = new MimeMessage(session);
+            mess.setFrom(new InternetAddress(fromEmail));
+            mess.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
+            mess.setSubject(subject);
+            mess.setText(content);
             Transport.send(mess);
             test = true;
         } catch (Exception e) {
